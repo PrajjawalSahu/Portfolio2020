cf16191567aec5c677d50f27e44c1c5037a09287
PrajjawalSahu <nikku2001.sahu@gmail.com>
2020-09-30 14:43:51 +0530

    Updates

---
+++ precache-manifest.e745ab8536311642bda61557da02d00f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1522c4085d6cb38d288ad9aa10a2c610",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "a85a740521da946d6759",
+    "url": "/portfolio2020/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "25d83ec239349d7c93cd",
+    "url": "/portfolio2020/static/css/main.36b59169.chunk.css"
+  },
+  {
+    "revision": "a85a740521da946d6759",
+    "url": "/portfolio2020/static/js/2.4b3b38d0.chunk.js"
+  },
+  {
+    "revision": "107d35b48d537bad98f062c32175d0f7",
+    "url": "/portfolio2020/static/js/2.4b3b38d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d83ec239349d7c93cd",
+    "url": "/portfolio2020/static/js/main.d630d3e4.chunk.js"
+  },
+  {
+    "revision": "37da5e84a589ad84aaaa",
+    "url": "/portfolio2020/static/js/runtime-main.ca6f5c64.js"
+  }
+]);
